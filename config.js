48f9a5df53f3db573a876a0c4c7c11a55186b54e
Paul Okeke <donpaul120@gmail.com>
2020-07-24 16:01:23 +0100

    modified

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const config = {
   ROOT_DIR: __dirname,
-  URL_PORT: process.env.APP_PORT,
+  URL_PORT: process.env.PORT || process.env.APP_PORT,
   URL_PATH: 'http://localhost',
   BASE_VERSION: 'v2',
   CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
