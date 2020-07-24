6328de7306b48a8229228e2858f7d8f581e3d994
Paul Okeke <donpaul120@gmail.com>
2020-07-24 17:14:12 +0100

    removed redundant code

--- Context.js
+++ Context.js
@@ -4,14 +4,9 @@ const knexConfig = require('./knexfile');
 class Context {
 
     #database;
-    static #SWAPI_BASE_URL = "https://swapi.dev/api";
 
     constructor() {
         this.#database = KNEX(knexConfig['development']);
-    }
-
-    getSwapiBaseUrl(){
-        return Context.#SWAPI_BASE_URL;
     }
 
     getDatabase() {
